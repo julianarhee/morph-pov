2252dbb719e6fd8ff4ce85ec4c5915de3180c982
julianarhee <juliana.rhee@gmail.com>
2016-06-06 12:28:41 -0400

    fixed error in calculating norm, didnt have subtraction sign

--- matlab/sample_V1_features_euclid_neighbor.m
+++ matlab/sample_V1_features_euclid_neighbor.m
@@ -70,7 +70,8 @@ while 1
     
     curr_vect = load([source_root, fnames{curr_vect_idx}]);
     next_vect = load([source_root, fnames{next_vect_idx}]);
-    D = norm(curr_vect.featureVector', next_vect.featureVector'); % Get Euclidean distance between vec1 and curr_vect
+
+    D = norm(curr_vect.featureVector'-next_vect.featureVector'); % Get Euclidean distance between vec1 and curr_vect
     distance_vect = [distance_vect; D];
     
 %     start_idx = last_idx;
